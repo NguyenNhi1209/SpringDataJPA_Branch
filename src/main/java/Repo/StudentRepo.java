aa58ad1b38f7ec4c32d361dc17eb510c6a48bd49
HoangNhi <hoangnhi.120900@gmail.com>
2021-09-13 19:59:39 +0700

    git commit -m "native_branch"

--- src/main/java/Repo/StudentRepo.java
+++ src/main/java/Repo/StudentRepo.java
@@ -2,8 +2,22 @@ package Repo;
 
 import entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface StudentRepo extends JpaRepository<Student, Integer> {
+    @Query( value = "select * from student", nativeQuery = true)
+    List<Student> getAll();
+
+    @Transactional
+    @Modifying
+    @Query(value = "update student set name = ?2 where id = ?1", nativeQuery = true)
+    void updateStudent(int id, String name);
+
+
 }
--- src/test/java/com/example/test1/Test1ApplicationTests.java
+++ src/test/java/com/example/test1/Test1ApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.test1;
 
+import Repo.StudentRepo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Test1ApplicationTests {
 
+    // native_branch
+    private StudentRepo studentRepo;
     @Test
-    void contextLoads() {
+    void testRead() {
+        studentRepo.getAll();
     }
-
+    @Test
+    void testUpdate() {
+        studentRepo.updateStudent(1, "abc");
+    }
 }
