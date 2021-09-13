5a8b83804f4fb448a5273ae61cff627af9776d6a
HoangNhi <hoangnhi.120900@gmail.com>
2021-09-13 20:09:40 +0700

    git commit -m "jpql_branch"

--- src/main/java/Repo/StudentRepo.java
+++ src/main/java/Repo/StudentRepo.java
@@ -2,8 +2,16 @@ package Repo;
 
 import entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StudentRepo extends JpaRepository<Student, Integer> {
+
+    @Query("select s from Student  s")
+    List<Student> getAll();
+
+
 }
--- src/test/java/com/example/test1/Test1ApplicationTests.java
+++ src/test/java/com/example/test1/Test1ApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.test1;
 
+import Repo.StudentRepo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Test1ApplicationTests {
 
+    //jpql_branch
+    private StudentRepo studentRepo;
+
     @Test
-    void contextLoads() {
+    void testRead(){
+        studentRepo.getAll();
+
+    }
+
+    @Test
+    void testDelete(){
+        studentRepo.deleteAll();
     }
 
 }
