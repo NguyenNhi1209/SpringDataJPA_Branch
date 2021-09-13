215f8b49118a1b9b860d0d99d9c2ec7df6793bfc
HoangNhi <hoangnhi.120900@gmail.com>
2021-09-13 19:45:17 +0700

    git commit -m "repo_branch"

--- src/main/java/entity/Student.java
+++ src/main/java/entity/Student.java
@@ -8,4 +8,17 @@ public class Student {
     @Id
     private int id;
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
--- src/test/java/com/example/test1/Test1ApplicationTests.java
+++ src/test/java/com/example/test1/Test1ApplicationTests.java
@@ -1,13 +1,46 @@
 package com.example.test1;
 
+import Repo.StudentRepo;
+import ch.qos.logback.core.CoreConstants;
+import entity.Student;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Test1ApplicationTests {
 
+    // repo_branch
+
+    private StudentRepo studentRepo;
+
     @Test
-    void contextLoads() {
+    void testSave(){
+        Student s = new Student(1,"nhi");
+        studentRepo.save(s);
     }
+
+    @Test
+    void testRead(){
+        System.out.println(studentRepo.findAll());
+    }
+
+    @Test
+    void testUpdate(){
+       Student s = studentRepo.findAll().get(0);
+       s.setName("nhi 123");
+       studentRepo.save(s);
+    }
+
+    @Test
+    void testDelete(){
+        studentRepo.deleteAll();
+    }
+
+
+
+
+
+
 
 }
